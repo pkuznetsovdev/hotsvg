c3cf43ec8e947a0b8d32053adfff7b4955a30ea4
pkuznetsov <aimanptz@yandex.ru>
2019-11-22 17:32:52 +0300

    code refactor

--- src/components/sprite-input/sprite-input.tsx
+++ src/components/sprite-input/sprite-input.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { IUploadedFiles } from '../../interfaces';
-import { onUploadData } from '../../services/loader-service';
+import { connect } from 'react-redux';
 
-const SpriteInput = () => {
+/*  Utils */
+import { loadData } from '../../actions';
+import { IUploadedFiles } from '../../interfaces';
+
+type Props = {
+  loadData: (a: IUploadedFiles) => void
+};
+
+const SpriteInput = (props: Props) => {
 
   const onChangeHandler = (e: React.ChangeEvent) => {
     const { files } = e.target as HTMLInputElement;
     if (!files) return;
-    let newFiles: IUploadedFiles = [];
 
-    for (let i = 0, filesLength = files.length; i < filesLength; i++) {
-      newFiles.push(files[i]);
-    }
-
-    onUploadData(newFiles);
+    props.loadData(Array.from(files));
   };
 
   return (
@@ -24,10 +26,14 @@ const SpriteInput = () => {
              multiple
              hidden
              onChange={onChangeHandler}
-             onClick={({target}) => (target as HTMLInputElement).value = ''}
+             onClick={({ target }) => (target as HTMLInputElement).value = ''}
       />
     </label>
   );
 };
 
-export default SpriteInput;
+const mapDispatchToProps = (dispatch: any) => ({
+  loadData: loadData(dispatch),
+});
+
+export default connect(null, mapDispatchToProps)(SpriteInput);
