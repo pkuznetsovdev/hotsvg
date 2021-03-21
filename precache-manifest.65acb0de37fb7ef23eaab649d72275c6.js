54390f070df49fb70ed787e99b711a6696fc180f
Pavel Kuznetsov <p.kuznetsov@andersenlab.com>
2021-03-21 13:16:10 +0300

    Updates

---
+++ precache-manifest.65acb0de37fb7ef23eaab649d72275c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de3a936258a6de6cc5a15b0dbf9f290",
+    "url": "/hotsvg/index.html"
+  },
+  {
+    "revision": "327be3f9a021c9450ef7",
+    "url": "/hotsvg/static/css/main.e2113972.chunk.css"
+  },
+  {
+    "revision": "63322449bec5ff8e4a5e",
+    "url": "/hotsvg/static/js/2.613e67dc.chunk.js"
+  },
+  {
+    "revision": "327be3f9a021c9450ef7",
+    "url": "/hotsvg/static/js/main.507607c6.chunk.js"
+  },
+  {
+    "revision": "cacb8635ba8e11559416",
+    "url": "/hotsvg/static/js/runtime-main.ef8c8229.js"
+  }
+]);
